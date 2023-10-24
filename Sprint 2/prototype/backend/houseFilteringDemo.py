f61ccdb1827d1bebc47200c8fb9559697a96e800
Shah-MU <145060887+Shah-MU@users.noreply.github.com>
2023-10-24 15:46:52 -0400

    Update houseFilteringDemo.py
    Added Login functionality and crud demo to the application
    
    Note login info is stored in userdata.csv

--- Sprint 2/prototype/backend/houseFilteringDemo.py
+++ Sprint 2/prototype/backend/houseFilteringDemo.py
@@ -3,19 +3,14 @@ import pandas as pd
 from st_aggrid import AgGrid, GridUpdateMode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 
-image1 = 'https://www.forbes.com/advisor/wp-content/uploads/2022/10/what-is-a-townhouse.jpeg.jpg'
-image2 = 'https://simpleshowing.ghost.io/content/images/2023/01/GetMedia-1.jpeg'
+# Read user data from 'userdata.csv' file
+user_data = pd.read_csv('userdata.csv')
 
-data = {
-    'house address': ['House 1', 'House 2'],
-    'house type': ['Townhouse', 'Split Level'],
-    'number of rooms': ['6', '7'],
-    'number of bathrooms': ['2', '3'],
-    'image': [image1, image2],
-    'broker_email': ['johnterry@gmail.com', 'terrencewallace@hotmail.com']
-}
+valid_usernames = set(user_data['username'])
+user_passwords = dict(zip(user_data['username'], user_data['password']))
 
-df = pd.DataFrame(data)
+# Load data from data.csv file
+df = pd.read_csv('data.csv')
 
 # Create a copy of the DataFrame without the 'email' column
 df_filtered = df[['house address', 'house type', 'number of rooms', 'number of bathrooms', 'image']]
@@ -28,75 +23,151 @@ gridoptions = gd.build()
 # Initialize the visit request DataFrame
 visit_request_df = pd.DataFrame(columns=["b_email", "property", "fname", "lname", "pnumber", "email", "date_time"])
 
-# Dropdown filter for house type
-selected_house = st.selectbox('Select Property Type', ['All'] + list(df_filtered['house type'].unique()))
+selected_tab = st.sidebar.radio("Select a tab:", ["View Data", "CRUD Operations"], key="tab_selection")
 
-# Display the ag-Grid with house information
-st.write("## List of Properties")
+# User login interface
+username = st.sidebar.text_input("Username")
+password = st.sidebar.text_input("Password", type='password')
 
-if selected_house != 'All':
-    filtered_df = df_filtered[df_filtered['house type'] == selected_house]
+# Authentication logic
+if st.sidebar.button("Login"):
+    if username in valid_usernames and password == user_passwords[username]:
+        st.sidebar.success(f"Welcome, {username}!")
+        st.session_state.logged_in = True
+    else:
+        st.sidebar.error("Invalid credentials. Please try again.")
+        st.session_state.logged_in = False
+
+# Add a signout button in the sidebar
+if getattr(st.session_state, 'logged_in', False):
+    if st.sidebar.button("Sign Out"):
+        st.session_state.logged_in = False
+        st.sidebar.success("You have been successfully signed out.")
+
+    if selected_tab == "View Data":
+        # Dropdown filter for house type
+        selected_house = st.selectbox('Select Property Type', ['All'] + list(df_filtered['house type'].unique()))
+
+        # Display the ag-Grid with house information
+        st.write("## List of Properties")
+
+        if selected_house != 'All':
+            filtered_df = df_filtered[df_filtered['house type'] == selected_house]
+        else:
+            filtered_df = df_filtered
+
+        grid_table = AgGrid(filtered_df, height=250, gridOptions=gridoptions,
+                            update_mode=GridUpdateMode.SELECTION_CHANGED)
+
+        if grid_table['selected_rows']:
+            selected_row = grid_table['selected_rows'][0]
+            image_column, description_column = st.columns(2)
+
+            with image_column:
+                st.image(selected_row['image'], caption='Selected Image', use_column_width=True)
+
+            with description_column:
+                st.write(f"House Address: {selected_row['house address']}\n"
+                         f"\nHouse Type: {selected_row['house type']}\n"
+                         f"\nNumber of Rooms: {selected_row['number of rooms']}\n"
+                         f"\nNumber of Bathrooms: {selected_row['number of bathrooms']}")
+
+            if st.button("Request Visit"):
+
+                # Use st.form to wrap all the inputs
+                with st.form(key='visit_request_form'):
+                    fname = st.text_input("First Name")
+                    lname = st.text_input("Last Name")
+                    pnumber = st.text_input("Phone Number")
+                    email = st.text_input("Email")
+                    date = st.date_input("Visit Date", value="today", key='visit_date')
+                    time = st.time_input("Visit Time", value="now", key='visit_time')
+                    submitted = st.form_submit_button("Submit Request")
+
+                if submitted:
+                    # Process the form submission
+                    broker_email = selected_row['broker_email']
+                    new_request = pd.DataFrame({
+                        "b_email": [broker_email],
+                        "property": [selected_row['house address']],
+                        "fname": [fname],
+                        "lname": [lname],
+                        "pnumber": [pnumber],
+                        "email": [email],
+                        "date": [date],
+                        "time": [time]
+                    })
+
+                    # Append the new request to the visit request DataFrame
+                    visit_request_df = visit_request_df.append(new_request, ignore_index=True)
+
+                    st.write("Request submitted successfully!")
+
+        # Display the table with visit requests
+        st.write("## Visit Requests")
+        if not visit_request_df.empty:
+            st.dataframe(visit_request_df, height=250)
+        else:
+            st.info("No visit requests yet.")
+    elif selected_tab == "CRUD Operations":
+        # CRUD operations
+
+        st.write("### CRUD Operations")
+
+        # CREATE
+        st.write("#### Create")
+        new_house_address = st.text_input("House Address")
+        new_house_type = st.text_input("House Type")
+        new_num_rooms = st.text_input("Number of Rooms")
+        new_num_bathrooms = st.text_input("Number of Bathrooms")
+        new_image = st.text_input("Image URL")
+        if st.button("Add New Row"):
+            new_row = pd.DataFrame([{
+                'house address': new_house_address,
+                'house type': new_house_type,
+                'number of rooms': new_num_rooms,
+                'number of bathrooms': new_num_bathrooms,
+                'image': new_image,
+                'status': 'Pending'
+            }])
+            df = pd.concat([df, new_row], ignore_index=True)
+            df.to_csv('data.csv', index=False)
+
+        # UPDATE
+        st.write("#### Update")
+        row_to_update = st.number_input("Row Index to Update", min_value=0, max_value=len(df) - 1, step=1, value=0)
+        update_house_address = st.text_input("House Address", value=df.at[row_to_update, 'house address'])
+        update_house_type = st.text_input("House Type", value=df.at[row_to_update, 'house type'])
+        update_num_rooms = st.text_input("Number of Rooms", value=df.at[row_to_update, 'number of rooms'])
+        update_num_bathrooms = st.text_input("Number of Bathrooms", value=df.at[row_to_update, 'number of bathrooms'])
+        update_image = st.text_input("Image URL", value=df.at[row_to_update, 'image'])
+        if st.button("Update Row"):
+            df.at[row_to_update, 'house address'] = update_house_address
+            df.at[row_to_update, 'house type'] = update_house_type
+            df.at[row_to_update, 'number of rooms'] = update_num_rooms
+            df.at[row_to_update, 'number of bathrooms'] = update_num_bathrooms
+            df.at[row_to_update, 'image'] = update_image
+            df.to_csv('data.csv', index=False)
+
+        # DELETE
+        st.write("#### Delete")
+        row_to_delete = st.number_input("Row Index to Delete", min_value=0, max_value=len(df) - 1, step=1, value=0)
+        if st.button("Delete Row"):
+            df.drop(index=row_to_delete, inplace=True)
+            df.reset_index(drop=True, inplace=True)
+            df.to_csv('data.csv', index=False)
+
+
 else:
-    filtered_df = df_filtered
+    # If not logged in, do not display other tabs
+    st.warning("Please log in to access the application.")
 
-grid_table = AgGrid(filtered_df, height=250, gridOptions=gridoptions, update_mode=GridUpdateMode.SELECTION_CHANGED)
 
-if grid_table['selected_rows']:
-    selected_row = grid_table['selected_rows'][0]
-    image_column, description_column = st.columns(2)
-
-    with image_column:
-        st.image(selected_row['image'], caption='Selected Image', use_column_width=True)
-
-    with description_column:
-        st.write(f"House Address: {selected_row['house address']}\n"
-                 f"\nHouse Type: {selected_row['house type']}\n"
-                 f"\nNumber of Rooms: {selected_row['number of rooms']}\n"
-                 f"\nNumber of Bathrooms: {selected_row['number of bathrooms']}")
-
-    if st.button("Request Visit"):
-
-        # Use st.form to wrap all the inputs
-        with st.form(key='visit_request_form'):
-            fname = st.text_input("First Name")
-            lname = st.text_input("Last Name")
-            pnumber = st.text_input("Phone Number")
-            email = st.text_input("Email")
-            date = st.date_input("Visit Date", value="today", key='visit_date')
-            time = st.time_input("Visit Time", value="now", key='visit_time')
-            submitted = st.form_submit_button("Submit Request")
-
-        if submitted:
-            # Process the form submission
-            broker_email = selected_row['broker_email']
-            new_request = pd.DataFrame({
-                "b_email": [broker_email],
-                "property": [selected_row['house address']],
-                "fname": [fname],
-                "lname": [lname],
-                "pnumber": [pnumber],
-                "email": [email],
-                "date": [date],
-                "time": [time]
-            })
-
-            # Append the new request to the visit request DataFrame
-            visit_request_df = visit_request_df.append(new_request, ignore_index=True)
-
-            st.write("Request submitted successfully!")
-
-# Display the table with visit requests
-st.write("## Visit Requests")
-if not visit_request_df.empty:
-    st.dataframe(visit_request_df, height=250)
-else:
-    st.info("No visit requests yet.")
-
-# Boiler plate code:
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+    # Boilerplate code for hiding Streamlit style
+    hide_streamlit_style = """
+         <style>
+         #MainMenu {visibility: hidden;}
+         footer {visibility: hidden;}
+         </style>
+     """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
