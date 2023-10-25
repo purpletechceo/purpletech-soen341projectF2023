ea9361b5e8e13608266a34e6578bd7765c7ee487
astra47 <g_olanoo@live.concordia.ca>
2023-10-25 18:31:02 -0400

    Added requirements file the root level
    Change needed for CI script

---
+++ Sprint 2/Implemented Features/Backend/test_initial.py
@@ -0,0 +1,91 @@
+import pytest
+import streamlit as st
+import pandas as pd
+from st_aggrid import AgGrid, GridUpdateMode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+
+"""
+st.title('Counter Example')
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+increment = st.button('Increment')
+if increment:
+    st.session_state.count += 1
+
+st.write('Count = ', st.session_state.count)
+"""
+
+class Test:
+    """
+    # Test case for checking if the app title is set correctly
+    def test_app_title(self):
+        assert googleLogin.st.title == "Google Login App"
+
+    # Test case for the initial state of the access_token in session_state
+    def test_initial_access_token(self):
+        assert 'access_token' not in googleLogin.st.session_state
+        assert googleLogin.st.session_state.access_token is None
+
+    # Test case for the login button
+    def test_login_button(self):
+        assert googleLogin.google_button.label == "Login with Google"
+
+    # Test case for the sign-out button
+    def test_sign_out_button(self):
+        assert googleLogin.sign_out_button.label == "Sign Out"
+    """
+
+    # Acceptance Test 1: Search
+    def test_search_correct_location(self):
+        pass
+
+    # Acceptance Test 2: Filter
+    def test_filter_results(self):
+        pass
+
+    # Acceptance Test 1: Options checking by brokers
+    def broker_options(self):
+        pass
+
+    # Acceptance Test 2: New window operations
+    def new_window_operations(self):
+        pass
+
+    # Acceptance Test 1: Data checking for inspecting a property
+    def inspect_property(self):
+        pass
+
+    # Acceptance Test 2: Receipt of confirmation
+    def confirmation_email(self):
+        pass
+
+    # Acceptance Test 1: Listing controls
+    def listing_controls(self):
+        pass
+
+    # Acceptance Test 2: Editing controls
+    def editing_controls(self):
+        pass
+
+    # Helper functions to simulate the system
+    def search(self, location):
+        pass
+
+    def filter_properties(self, price_range, bedrooms, amenities):
+        pass
+
+    def perform_broker_option(self, option):
+        pass
+
+    def perform_new_window_operations(self, property_data):
+        pass
+
+    def inspect_property(self, date, time):
+        pass
+
+    def confirmation_emails(self, submit_button):
+        pass
+
+    def perform_listing_control(self, action):
+        pass
