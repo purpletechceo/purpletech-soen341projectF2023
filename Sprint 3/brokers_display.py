ea2739e2f604063b8d978e82fc71daf2e4811ba7
Oyeyimikaa <145073419+Oyeyimikaa@users.noreply.github.com>
2023-11-09 07:11:38 -0500

    Update brokers_display.py with select broker from filtered list

--- Sprint 3/brokers_display.py
+++ Sprint 3/brokers_display.py
@@ -20,3 +20,24 @@ search_keyword = st.text_input('Keyword Search', '')
 # Filter the DataFrame based on the search keyword
 filtered_df = df_filtered[df_filtered.apply(lambda row: any(search_keyword.lower() in str(cell).lower() for cell in row), axis=1 )]
 
+
+# Filter the DataFrame based on the search keyword
+filtered_df = df_filtered[df_filtered.apply(lambda row: any(search_keyword.lower() in str(cell).lower() for cell in row), axis=1 )]
+
+# Display the filtered data
+st.write("## List of Brokers")
+
+if len(filtered_df) > 0:
+    grid_table = AgGrid(filtered_df, columns_auto_size_mode=ColumnsAutoSizeMode.FIT_CONTENTS, height=250,
+                        gridOptions=gridoptions, update_mode=GridUpdateMode.SELECTION_CHANGED)
+    if grid_table['selected_rows']:
+        selected_row = grid_table['selected_rows'][0]
+        first_name = selected_row['FirstName']
+        last_name = selected_row['LastName']
+        phone = selected_row['Phone']
+        email = selected_row['Email']
+        license_number = selected_row['License Number']
+        agency = selected_row['Agency']
+        image_url = selected_row['ImageURL']
+        
+
