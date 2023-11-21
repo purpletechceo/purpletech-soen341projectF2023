08910683162b3a49bfa3fce249c8fc0ee8d48699
astra47 <g_olanoo@live.concordia.ca>
2023-11-21 08:47:41 -0500

    Resolving merge conflict

--- Sprint 3/additionalFeatures.py
+++ Sprint 3/additionalFeatures.py
@@ -68,13 +68,8 @@ property_type = st.sidebar.selectbox("Property Type", ["House", "Apartment", "Co
 bedrooms = st.sidebar.selectbox("Bedrooms", [1, 2, 3, 4, 5])
 
 # Feature 2: Interactive Maps using Google Maps
-<<<<<<< HEAD
-st.header("Sprint 3-4: Potential Nice-to-Have Features")
-st.write("These features are only partially implemented, the purpose is to discuss these potential improvements with the team prior to Sprint 4.")
-=======
 st.header("Upcoming Sprint 4: Potential Nice-to-Have Features")
 st.write("Exploring potential nice-to-have features")
->>>>>>> main
 st.header("Property Listings on Map")
 
 # Display the map with property markers (updated to show filtered properties)
